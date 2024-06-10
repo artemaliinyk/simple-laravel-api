26212ac62fc52a582349cf7e6ce29a4b441254d8
Artem <shotty.log@gmail.com>
2024-06-10 15:11:09 +0300

    Save the data to the `submissions` table via Job Queue

--- app/Http/Controllers/SubmissionController.php
+++ app/Http/Controllers/SubmissionController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\SubmissionRequest;
+use App\Jobs\ProcessSubmission;
+use Illuminate\Http\JsonResponse;
 
 class SubmissionController extends Controller
 {
-    public function submit(SubmissionRequest $request)
+    public function submit(SubmissionRequest $request): JsonResponse
     {
-        //
+        ProcessSubmission::dispatch($request->all());
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Submission received and will be processed shortly.'
+        ]);
     }
 }
---
+++ app/Jobs/ProcessSubmission.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Submission;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ProcessSubmission implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(protected array $data) {}
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $submission = Submission::create($this->data);
+    }
+}
